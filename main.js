5c02b12254d9d172538b859f0c4dfd14ce8ce057
Tejasri <ep23btech11018@iith.ac.in>
2025-02-21 15:18:20 +0530

    Modified

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
 // change navbar styles on scroll
 
-window.addEventListener('scroll',()=>{
-    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0)
-})
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
 
 // Show/Hide nav menu buton
-const menu = document.querySelector(".nav__menu")
-const menubtn = document.querySelector("#open-menu-btn")
-const closebtn = document.querySelector("#close-menu-btn")
+const menu = document.querySelector(".nav__menu");
+const menubtn = document.querySelector("#open-menu-btn");
+const closebtn = document.querySelector("#close-menu-btn");
 
-menubtn.addEventListener('click', () => {
-    menu.style.display = "flex";
-    closebtn.style.display = "inline-block"
-    menubtn.style.display = "none"
+menubtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closebtn.style.display = "inline-block";
+  menubtn.style.display = "none";
+});
 
-})
+closebtn.addEventListener("click", () => {
+  menu.style.display = "none";
+  closebtn.style.display = "none";
+  menubtn.style.display = "inline-block";
+});
 
-closebtn.addEventListener('click', () => {
-    menu.style.display = "none";
-    closebtn.style.display = "none"
-    menubtn.style.display = "inline-block"
+// Load different pages
+function loadPage(page) {
+  fetch(page)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("content").innerHTML = data;
+    })
+    .catch((error) => console.error("Error loading the page:", error));
+}
 
-})
+// function loadPage(page) {
+//   document.getElementById("content").innerHTML =
+//     "<object type='text/html' data='" +
+//     page +
+//     "' style='width:100%;height:100vh;'></object>";
+// }
+
+// Load home page by default
+window.onload = function () {
+  loadPage("home.html");
+};
